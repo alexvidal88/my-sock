6b461d89968ef27f453aec461babee537d951374
Alex Vidal <alexr.vidalh@gmail.com>
2021-06-29 20:51:50 +1000

    Add dotenv - Protect my secret data in .env file

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,25 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: :destroy
 
+  def index
+    @sock = Sock.find(params[:sock_id])
+    @bookings = Booking.all
+  end
+
+  def new
+    @sock = Sock.find(params[:sock_id])
+    @booking = Booking.new
+  end
+
   def create
     @booking = Booking.new(booking_params)
-    @sock = Sock.find(params[:list_id])
+    @sock = Sock.find(params[:sock_id])
+    @booking.user = current_user
     @booking.sock = @sock
     if @booking.save
       redirect_to sock_path(@sock)
     else
-      render 'socks/:sock_id/bookings'
+      render :new
     end
   end
 
@@ -25,7 +36,7 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:comment, :movie_id)
+    params.require(:booking).permit(:start_date, :end_date)
   end
 
   def set_booking
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :socks, except: [:edit, :update] do
-    resources :bookings, only: [:update, :create]
+    resources :bookings
   end
   resources :bookings, only: :destroy
 end
