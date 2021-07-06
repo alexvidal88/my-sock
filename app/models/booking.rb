f73e2530430d6134ed373f13dd655630dc3f4bca
Armand Leleu Estrada <armandleleu47@gmail.com>
2021-07-06 21:22:46 +1000

    created reviews, model controller view/new

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :sock
+  has_many :reviews
 
   validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
