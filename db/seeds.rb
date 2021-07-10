7875baeac91e18760ce28ee52f78352412635c89
Alex Vidal <alexr.vidalh@gmail.com>
2021-07-10 12:36:11 +1000

    seed fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,13 +20,14 @@ new_socks['socks'].each do |new_sock|
     name: new_sock['name'],
     price: new_sock['price'],
     description: new_sock['description'],
-    address: new_sock["address"]
+    address: new_sock["address"],
+    user_id: user.id
   )
 
 
  file = URI.open(new_sock['photo'])
  created_sock.photo.attach(io: file, filename: "#{created_sock.name}.jpg", content_type: 'image/jpg')
- created_sock.user = user
+ # created_sock.user = User.last
  created_sock.save!
 end
 
